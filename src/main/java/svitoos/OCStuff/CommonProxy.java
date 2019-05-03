bf8e1e3245e5253c78183e88a3115e92a1285d9e
svitoos <svitoos@gmail.com>
2019-05-03 20:39:20 +0300

    Добавлен рецепт для Ultimate Navigation Upgrade

--- src/main/java/svitoos/OCStuff/CommonProxy.java
+++ src/main/java/svitoos/OCStuff/CommonProxy.java
@@ -6,6 +6,8 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 
 public class CommonProxy {
 
@@ -15,6 +17,23 @@ public class CommonProxy {
     OCStuff.itemUltimateNavigationUpgrade = new UpgradeUltimateNavigationItem();
     GameRegistry.registerItem(OCStuff.itemUltimateNavigationUpgrade, "ultimateNavigationUpgrade");
     API.driver.add(new UpgradeUltimateNavigationDriver());
+    if (Config.regRecipeUpgradeUltimateNavigation) {
+      GameRegistry.addRecipe(
+          new ItemStack(OCStuff.itemUltimateNavigationUpgrade),
+          "xgx",
+          "cic",
+          "xpx",
+          'g',
+          Items.map,
+          'x',
+          Items.gold_ingot,
+          'c',
+          li.cil.oc.api.Items.get("chip2").createItemStack(1),
+          'i',
+          Items.ender_eye,
+          'p',
+          li.cil.oc.api.Items.get("printedCircuitBoard").createItemStack(1));
+    }
   }
 
   public void postInit(FMLPostInitializationEvent e) {}
--- src/main/java/svitoos/OCStuff/Config.java
+++ src/main/java/svitoos/OCStuff/Config.java
@@ -7,9 +7,12 @@ import net.minecraftforge.common.config.Configuration;
 class Config {
   private static Configuration configuration;
 
+  static boolean regRecipeUpgradeUltimateNavigation;
+
   static void init(File file) {
     configuration = new Configuration(file, true);
     configuration.load();
+    regRecipeUpgradeUltimateNavigation = configuration.getBoolean("UpgradeUltimateNavigation", "recipe", true, "Add recipe for Ultimate Navigation Upgrade");
     configuration.save();
   }
 }
